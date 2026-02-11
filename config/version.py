87c0d4025db577be83566a3460b72b4997fcc9e6
jyppy77-hash <jyppy77@gmail.com>
2026-02-11 21:23:33 +0100

    Add files via upload

---
+++ config/version.py
@@ -0,0 +1,11 @@
+"""
+Configuration centralisée des versions LotoIA.
+Tous les fichiers du projet DOIVENT importer depuis ce module.
+"""
+
+APP_VERSION = "1.001"
+APP_NAME = "LotoIA"
+VERSION_DATE = "2025-02-11"
+
+# Alias pour compatibilité avec engine/version.py
+__version__ = APP_VERSION
